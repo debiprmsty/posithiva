4a79a6326be67994c2737bf79523e1f1ef80d0f1
Lanang-123 <lanangobey123@gmail.com>
2023-09-01 00:25:44 -0700

    theme asset

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color birutua = const Color(0xFF3779C7);
+Color birumuda = const Color(0xFF3D85DB);
+Color abu = const Color(0xFFF5F5F5);
+
+TextStyle lora = const TextStyle(fontFamily: 'Lora');
+TextStyle lato = const TextStyle(fontFamily: 'Lato');
+TextStyle poppins = const TextStyle(fontFamily: 'Poppins');
